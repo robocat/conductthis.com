7f7293282517805330cf20237e51920123bfd39f
Kristian Andersen <hello@kristian.co>
2016-11-14 16:34:50 +0100

    Remove docker

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var compression = require('compression');
 
 var app = express();
+app.use(compression());
 app.use(express.static('build'));
 
 app.listen(process.env.PORT || 3000);
